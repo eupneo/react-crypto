27f77e0cc61fc82e8fab943567b71f918936b29d
eupneo <eupneo@gmail.com>
2022-03-25 10:07:28 +0100

    Prova pipeline

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.coingecko.com/api/v3';
 axios.defaults.headers['Content-Type'] = 'application/json';
 
+
 // axios.interceptors.request.use(async conf => {
 //     // const state = store.getState();
 //     // let token = getUserToken(state);
